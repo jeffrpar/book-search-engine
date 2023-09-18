4ea5cd98daf155078b79bd5653ba1cf52d025c8f
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-18 16:51:55 -0500

    added SAVE_BOOK to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,3 +21,44 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+    }
+  }
+`;
