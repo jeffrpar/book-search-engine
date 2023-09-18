22c07bd0461b8a97eb9d488f541c7f44dc1da186
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-18 16:08:31 -0500

    added saveBook and removeBook mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 module.exports = {
   Query: {
@@ -13,26 +13,48 @@ module.exports = {
   },
   Mutation: {
     login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw new AuthenticationError('No profile with this email found!');
-        }
-  
-        const correctPw = await User.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect password!');
-        }
-  
-        const token = signToken(user);
-        return { token, user };
-      },
-      addUser: async (parent, { name, email, password }) => {
-        const user = await User.create({ name, email, password });
-        const token = signToken(user);
-  
-        return { token, user };
-      },
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+
+      const correctPw = await User.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(user);
+      return { token, user };
     },
-  };
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: args.input } },
+          { new: true },
+        );
+        return updatedUser;
+      }
+      throw new Error("User not found");
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: args.bookId } },
+          { new: true },
+        );
+        return updatedUser;
+      }
+      throw new Error("User not found");
+    },
+  },
+};
