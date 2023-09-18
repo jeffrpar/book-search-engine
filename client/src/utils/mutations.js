9257667ce3189a3dc00eee0f8945e65f743b1680
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-18 16:48:24 -0500

    added me query to queries.js, added LOGIN_USER to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,23 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
