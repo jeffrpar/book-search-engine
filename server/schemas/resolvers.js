303f70024fc209f9c6596c90f95ed5bbb28ffd60
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-18 15:57:00 -0500

    added proper imports to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
-const { User } = require("../models");
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 module.exports = {
   Query: {
