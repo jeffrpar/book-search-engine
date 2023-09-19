11af02239b6f3b5e0fac28c253910a91d7a64f3f
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-19 16:07:16 -0500

    fixed add user mutation to allow user signup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,44 +15,25 @@ module.exports = {
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
+    
       if (!user) {
         throw new AuthenticationError("No profile with this email found!");
       }
-
-      const correctPw = await User.isCorrectPassword(password);
-
+    
+      const correctPw = await bcrypt.compare(password, user.password);
+    
       if (!correctPw) {
         throw new AuthenticationError("Incorrect password!");
       }
-
+    
       const token = signToken(user);
       return { token, user };
     },
-    addUser: async (parent, { name, email, password }) => {
-      try {
-        // Check if a user with the same email already exists
-        const existingUser = await User.findOne({ email });
-
-        if (existingUser) {
-          throw new AuthenticationError('User with this email already exists');
-        }
-
-        // Hash the user's password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create the new user
-        const user = await User.create({ name, email, password: hashedPassword });
-
-        // Generate a token for the new user
-        const token = signToken(user);
-
-        return { token, user };
-      } catch (error) {
-        console.error(error);
-        throw new AuthenticationError('Unable to create user');
-      }
-    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },    
     saveBook: async (parent, args, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
